d73a86f93d4eab5a134dbc816672edac7ac3b5a5
Sebastian Kopf <skopf@caltech.edu>
2014-04-24 18:40:05 -0700

    move test for finding standards
    moved from conversions to data

--- tests/testthat/test-05-data.R
+++ tests/testthat/test-05-data.R
@@ -11,8 +11,10 @@ test_that("Testing that reference standards can be properly registered and retri
     expect_warning(register_standard(ratio(`2H` = 0.00015576, major = "1H", compound = "VSMOW")), "overwriting an existing standard")
     expect_warning(register_standard(ratio(`2H` = 0.00015575, major = "1H", compound = "VSMOW")), "overwriting an existing standard")
     expect_equal(get_standards(minor = "2H", major = "1H", name = "VSMOW")[[1]], ratio(`2H` = 0.00015575, major = "1H", compound = "VSMOW"))
+    expect_message(as.ratio(delta(`2H` = -100, major = "1H", ref = "VSMOW")), "Successfully found a matching standard")
     expect_equal(as.ratio(delta(`2H` = 100, major = "1H", ref = "VSMOW")), ratio(`2H` = 1.1 * 0.00015575, major = "1H"))
 })
+
 
 test_that("Testing that default values can be set properly", {
     # test defaults
