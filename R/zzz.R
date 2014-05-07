8ac2d604b06fb44f19e5f42300707d612fbef54f
Sebastian Kopf <skopf@caltech.edu>
2014-05-06 18:28:20 -0700

    correction for VSMOW
    from IUPAC

--- R/zzz.R
+++ R/zzz.R
@@ -12,7 +12,7 @@ setLoadActions(function(ns) {
     
     #  register default standards
     suppressWarnings({
-        register_standard(ratio(`2H` = 0.00015575, major = "1H", compound = "VSMOW"))
+        register_standard(ratio(`2H` = as.ratio(abundance(0.00015574)), major = "1H", compound = "VSMOW")) # from IUPAC report
         register_standard(ratio(`13C` = 0.011237, major = "12C", compound = "VPDB"))
         register_standard(ratio(`15N` = 0.003677, major = "14N", compound = "Air"))
         register_standard(ratio(`18O` = 0.0020052, major = "16O", compound = "VSMOW"))
